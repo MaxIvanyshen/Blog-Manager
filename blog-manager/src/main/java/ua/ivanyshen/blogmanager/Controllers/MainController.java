3a137923291bae548785946b2a3e36df17d8db4d
MaxIvanyshen <maxivanyshen@gmail.com>
2022-03-07 00:28:35 +0200

    some back-end done

--- blog-manager/src/main/java/ua/ivanyshen/blogmanager/Controllers/MainController.java
+++ blog-manager/src/main/java/ua/ivanyshen/blogmanager/Controllers/MainController.java
@@ -1,28 +1,16 @@
 package ua.ivanyshen.blogmanager.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import ua.ivanyshen.blogmanager.Models.User;
-import ua.ivanyshen.blogmanager.Models.UserRepo;
-
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
 public class MainController {
-
-    @Autowired
-    private UserRepo repo;
-    private String message = "";
-    private String username = "";
+    public static String username = "";
 
     @GetMapping("/")
     public String index(Model model) {     
@@ -34,58 +22,12 @@ public class MainController {
         model.addAttribute("username", username);
         return "index";
     }
-
-    @GetMapping("/signup")
-    public String signup(Model model) {
-        if(!username.equals("")) {
-            return "redirect:/homepage";
-        }
-        model.addAttribute("user", new User());
-        model.addAttribute("pass1", new String());
-        model.addAttribute("pass2", new String());
-        model.addAttribute("message", message);
-        return "signup";
-    }
-
-    @PostMapping("/signup")
-    public String signedUp(@ModelAttribute User user) {
-
-        User foundByEmail = repo.findByEmail(user.getEmail());
-        if(foundByEmail != null) {
-            message = "Email already taken";
-            return "redirect:/signup";
-        }
-
-        User foundByUsername = repo.findByUsername(user.getUsername());
-        if(foundByUsername != null) {
-            message = "Username already taken";
-            return "redirect:/signup";
-        }
-
-        if(user.getPass1().equals(user.getPass2())) {
-            user.setPassword(user.getPass2());
-        }
-        else {
-            message = "Passwords do not match";
-            return "redirect:/signup";
-        }
-
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        user.setPassword(encoder.encode(user.getPassword()));
-        repo.save(user);
-        System.out.println("User saved!");
-        return "redirect:/";
-    }
-
-
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
     
-    @GetMapping("/user/{id}/homepage")
+    @GetMapping("/user/{id}/home")
     public String homepage(@PathVariable("id") String id, Model model) {
         model.addAttribute("username", username);
         return "homepage";
     }
+
+    
 }
