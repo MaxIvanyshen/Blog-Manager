cac85f89c1ac7e59f7e041a8c6af749d859babdd
MaxIvanyshen <maxivanyshen@gmail.com>
2022-04-25 03:31:32 +0300

    change logout url to '/api/v1/logout'

--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/security/SpringSecurityConfig.java
+++ blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/security/SpringSecurityConfig.java
@@ -9,13 +9,19 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.DelegatingServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.SecurityContextServerLogoutHandler;
+import org.springframework.security.web.server.authentication.logout.WebSessionServerLogoutHandler;
 import ua.ivanyshen.blogmanagerapi.model.User.UserService;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 @EnableConfigurationProperties
@@ -28,7 +34,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/api/v1/blogs").authenticated()
+                .authorizeRequests().antMatchers("/api/v1/blogs/**").authenticated()
+                .and().logout().logoutUrl("/api/v1/logout")
                 .and().httpBasic()
                 .and().sessionManagement().disable();
     }
