410ad16990690451ddb5bbe72f557ed294c3b00e
MaxIvanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2022-04-27 00:26:12 +0300

    refactoring Blog.java

--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/model/Blog/Blog.java
+++ blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/model/Blog/Blog.java
@@ -103,21 +103,13 @@ public class Blog {
         String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                 + "0123456789"
                 + "abcdefghijklmnopqrstuvxyz";
-
-        // create StringBuffer size of AlphaNumericString
+        
         StringBuilder sb = new StringBuilder(n);
 
         for (int i = 0; i < n; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
 
-            // generate a random number between
-            // 0 to AlphaNumericString variable length
-            int index
-                    = (int)(AlphaNumericString.length()
-                    * Math.random());
-
-            // add Character one by one in end of sb
-            sb.append(AlphaNumericString
-                    .charAt(index));
+            sb.append(AlphaNumericString.charAt(index));
         }
 
         return sb.toString();
