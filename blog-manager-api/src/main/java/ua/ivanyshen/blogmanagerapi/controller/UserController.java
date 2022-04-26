a96c401c1f804e8ed42fb8c3e33c4a09e5b4fc3f
MaxIvanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2022-04-26 22:34:57 +0300

    remove comments cuz they are shit

--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/controller/UserController.java
+++ blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/controller/UserController.java
@@ -20,28 +20,22 @@ public class UserController {
         this.userService = userService;
     }
 
-    //get user rest api
     @GetMapping("/users/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
         User foundUser = userService.findById(id);
 
-        //create user response with data from found user
         UserResponse res = new UserResponse(foundUser);
-
-        //return user response
+        
         return ResponseEntity.ok(res);
     }
 
-    //create user rest api
     @PostMapping("/users")
     public UserResponse createNewUser(@RequestBody UserRequest req) {
-        //password encoder
+        
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         
-        //create user from the request
         User requestCreatedUser = new User(req.getUsername(), req.getEmail(), encoder.encode(req.getPassword()));
         
-        //return created user as user response
         return new UserResponse(userService.save(requestCreatedUser));
     }
 
@@ -50,17 +44,15 @@ public class UserController {
     public org.springframework.security.core.userdetails.User login(@RequestBody UserRequest req) {
         User foundUser = userService.findByUsername(req.getUsername());
 
-        //password encoder
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        if(encoder.matches(req.getPassword(), foundUser.getPassword())) { //if passwords match
-            return userService.loadUserByUsername(req.getUsername()); //log user in
+        if(encoder.matches(req.getPassword(), foundUser.getPassword())) {
+            return userService.loadUserByUsername(req.getUsername());
         }
-        //else 
+
         return null;
     }
 
-    //get current logged user username
     @GetMapping("/users/current")
     public ResponseEntity<String> getCurrentUserUsername() {
         String currentUserUsername = userService.getCurrentUserUsername();
