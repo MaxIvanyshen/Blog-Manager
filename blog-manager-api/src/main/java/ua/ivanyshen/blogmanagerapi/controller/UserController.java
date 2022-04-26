c9b839c184fce2c9b41d5d02145f77e643f22a19
MaxIvanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2022-04-26 09:54:50 +0300

    add comments to UserController

--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/controller/UserController.java
+++ blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/controller/UserController.java
@@ -13,57 +13,73 @@ import ua.ivanyshen.blogmanagerapi.model.User.*;
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
-    private final UserService service;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserService service) {
+    public UserController(UserService userService) {
         this.service = service;
     }
 
+    //get user rest api
     @GetMapping("/users/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
-        User u = service.findById(id);
+        User foundUser = service.findById(id);
 
+        //create user response with data from found user
         UserResponse res = new UserResponse();
-        res.setId(u.getId());
-        res.setUsername(u.getUsername());
-        res.setEmail(u.getEmail());
+        res.setId(foundUser.getId());
+        res.setUsername(foundUser.getUsername());
+        res.setEmail(foundUser.getEmail());
 
+        //return user response
         return ResponseEntity.ok(res);
     }
 
+    //create user rest api
     @PostMapping("/users")
     public UserResponse createNewUser(@RequestBody UserRequest req) {
+        //password encoder
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        User u = new User(req.getUsername(), req.getEmail(), encoder.encode(req.getPassword()));
-        return new UserResponse(service.save(u));
+        
+        //create user from the request
+        User requestCreatedUser = new User(req.getUsername(), req.getEmail(), encoder.encode(req.getPassword()));
+        
+        //return created user as user response
+        return new UserResponse(userService.save(requestCreatedUser));
     }
 
     @PostMapping("/login")
     //TODO: redo this thing
     public org.springframework.security.core.userdetails.User login(@RequestBody UserRequest req) {
-        User u = service.findByUsername(req.getUsername());
+        User foundUser = service.findByUsername(req.getUsername());
 
+        //password encoder
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-        if(encoder.matches(req.getPassword(), u.getPassword())) {
-            return service.loadUserByUsername(req.getUsername());
+        if(encoder.matches(req.getPassword(), u.getPassword())) { //if passwords match
+            return service.loadUserByUsername(req.getUsername()); //log user in
         }
-
+        //else 
         return null;
     }
 
+    //get current logged user username
     @GetMapping("/users/current")
     public ResponseEntity<String> currentUserUsername() {
+        //get current user data
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
         String username = "";
+        
+        boolean userLoggedIn = principal instanceof UserDetails;
 
-        if (principal instanceof UserDetails) {
+        if (userLoggedIn) {
             username = ((UserDetails)principal).getUsername();
         } else {
-            username = principal.toString();
+            username = principal.toString(); //returns "anonymousUser"
         }
+        
+        //return current user username as a response
         return ResponseEntity.ok(username);
     }
 
