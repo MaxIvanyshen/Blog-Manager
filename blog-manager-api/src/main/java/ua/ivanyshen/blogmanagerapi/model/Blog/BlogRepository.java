9ca32a180193fdc676cf96ff7a66d90658d0e8db
MaxIvanyshen <70772808+MaxIvanyshen@users.noreply.github.com>
2022-04-30 15:38:48 +0300

    Delete blog-manager-api directory

--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/model/Blog/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.ivanyshen.blogmanagerapi.model.Blog;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogRepository extends JpaRepository<Blog, String> {
-    public Blog findByName(String name);
-}
--- blog-manager-api/src/main/java/ua/ivanyshen/blogmanagerapi/model/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ua.ivanyshen.blogmanagerapi.model.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-    public User findByUsername(String username);
-}
